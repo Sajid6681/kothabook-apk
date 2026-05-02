f441dd0b5b214a474e39acc084bdca50c3d3df87
sajid6681 <sajid@kothabook.com>
2026-05-02 07:05:29 +0000

    Add release APK

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,13 @@ import 'core/theme/app_theme.dart';
 
 // 🚀 Screens Imports
 // TODO: তোমার ফাইল অনুযায়ী নিচের লোকেশনগুলো ঠিক করে নিও
-import 'screens/home/home_screen.dart'; // হোম স্ক্রিন ইম্পোর্ট
-// import 'features/splash/splash_screen.dart'; 
+import 'screens/intro/splash_screen.dart'; // হোম স্ক্রিন ইম্পোর্ট
+// import 'features/splash/splash_screen.dart';
 
 void main() async {
   // ১. ফ্লাটার ইঞ্জিন স্টার্ট করা
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   // ২. Multi-language (লোকালাইজেশন) ইঞ্জিন স্টার্ট করা
   await EasyLocalization.ensureInitialized();
 
@@ -21,7 +21,9 @@ void main() async {
     EasyLocalization(
       supportedLocales: const [Locale('en'), Locale('bn')], // ইংরেজি ও বাংলা
       path: 'assets/translations', // 📁 যেখানে ভাষার JSON ফাইলগুলো থাকবে
-      fallbackLocale: const Locale('en'), // কোনো ভাষা খুঁজে না পেলে ডিফল্ট ইংরেজি
+      fallbackLocale: const Locale(
+        'en',
+      ), // কোনো ভাষা খুঁজে না পেলে ডিফল্ট ইংরেজি
       child: const KothaBookApp(),
     ),
   );
@@ -34,20 +36,20 @@ class KothaBookApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'KothaBook',
-      debugShowCheckedModeBanner: false, // 🚀 ডানপাশের DEBUG ব্যানারটি রিমুভ করা হলো
-      
+      debugShowCheckedModeBanner:
+          false, // 🚀 ডানপাশের DEBUG ব্যানারটি রিমুভ করা হলো
       // 🌍 Multi-language কানেকশন
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
       locale: context.locale,
 
       // 🎨 স্মার্ট থিম ইঞ্জিন কানেকশন (ম্যাজিক!)
-      themeMode: ThemeMode.system, // ফোনের সেটিংস অনুযায়ী অটোমেটিক ডার্ক/লাইট মোড হবে!
+      themeMode:
+          ThemeMode.system, // ফোনের সেটিংস অনুযায়ী অটোমেটিক ডার্ক/লাইট মোড হবে!
       theme: AppTheme.lightTheme, // লাইট মোডের ডিজাইন
       darkTheme: AppTheme.darkTheme, // ডার্ক মোডের ডিজাইন
-      
       // 🚀 প্রথম স্ক্রিন (এখানে তুমি চাইলে Splash Screen দিতে পারো)
-      home: const HomeScreen(), 
+      home: const SplashScreen(),
     );
   }
 }
